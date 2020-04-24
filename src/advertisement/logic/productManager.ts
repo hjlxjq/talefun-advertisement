d2228c9c821da1192f12a20d4fbb52e00a6a669b
hjl@Talefun <jianlong801@gmail.com>
2020-04-24 15:27:40 +0800

    应用下打包参数和平台参数统一接口updupdupd

--- src/advertisement/logic/productManager.ts
+++ src/advertisement/logic/productManager.ts
@@ -261,7 +261,6 @@ export default class ProductManagerLogic extends AMLogic {
             value: {
                 string: true,       // 字段类型为 String 类型
                 trim: true,         // 字段需要 trim 处理
-                required: true,     // 字段必填
                 method: 'POST'       // 指定获取数据的方式
             }
         };
