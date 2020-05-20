349828af5d7180744b02f81ffd209c95efe7725c
hjl@Talefun <jianlong801@gmail.com>
2020-05-20 14:15:15 +0800

    upd

--- src/advertisement/logic/dispatchManager.ts
+++ src/advertisement/logic/dispatchManager.ts
@@ -2243,7 +2243,6 @@ export default class DispatchManagerLogic extends AMLogic {
             subloader: {
                 string: true,       // 字段类型为 String 类型
                 trim: true,         // 字段需要 trim 处理
-                required: true,     // 字段必填
                 method: 'POST'       // 指定获取数据的方式
             },
             ecpm: {
